ae70ced780fa03becc636739d1b716522a6b7674
Catalina <caliste@fen.uchile.cl>
2020-07-23 21:04:27 -0400

    clases

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#one'
-  get 'pages/one'
-  get 'pages/two'
-  get 'pages/three'
+  get '/one', to: 'pages#one'
+  get '/two', to: 'pages#two'
+  get '/three', to: 'pages#three'
 end
